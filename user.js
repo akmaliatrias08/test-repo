533d42fbec95fc34891203aedbad7164726636db
ReynerWL <reynerwl2018@gmail.com>
2023-10-23 12:09:16 +0700

    chore: Hello Reyner

--- user.js
+++ user.js
@@ -1,9 +1,8 @@
 console.log("HELLO JUA !!!")
 console.log("HELLO INI JUA LAGIII !!!")
 console.log("HELLO !!!")
-<<<<<<< HEAD
 console.log(" Hello I am Fauzi again!");
-=======
 console.log(" Hello I am Fauzi");
 console.log("cecil")
->>>>>>> 843eb0f80e4e6a6d9e74736c33f162533a9f4790
+console.log("ReynerWL");
+console.log("HI Rey");
