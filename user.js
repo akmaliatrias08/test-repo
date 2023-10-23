1efb623cd35267fb1e0072e11585aa1a197f532b
TyoKuncoro <tyodignitas@gmail.com>
2023-10-23 12:10:27 +0700

    Chore: add log

--- user.js
+++ user.js
@@ -1,7 +1,8 @@
-console.log("HELLO JUA !!!")
-console.log("HELLO INI JUA LAGIII !!!")
-console.log("HELLO !!!")
+console.log("HELLO JUA !!!");
+console.log("HELLO INI JUA LAGIII !!!");
+console.log("HELLO !!!");
 console.log(" Hello I am Fauzi");
-console.log("cecil")
+console.log("cecil");
 console.log(" Hello I am Danar");
-console.log("Danar")
+console.log("Danar");
+console.log("dignitas");
