d864d7de8c781ded965abdd6151902fcfd92e164
TyoKuncoro <tyodignitas@gmail.com>
2023-10-23 12:28:39 +0700

    add log

--- user.js
+++ user.js
@@ -1,17 +1,14 @@
-<<<<<<< HEAD
 console.log("HELLO JUA !!!");
 console.log("HELLO INI JUA LAGIII !!!");
 console.log("HELLO !!!");
-=======
-console.log("HELLO JUA !!!")
-console.log("HELLO INI JUA LAGIII !!!")
-console.log("HELLO !!!")
+console.log("HELLO JUA !!!");
+console.log("HELLO INI JUA LAGIII !!!");
+console.log("HELLO !!!");
 console.log(" Hello I am Fauzi again!");
 console.log(" Hello I am Fauzi");
-console.log("cecil")
+console.log("cecil");
 console.log("ReynerWL");
 console.log("HI Rey");
->>>>>>> refs/remotes/origin/main
 console.log(" Hello I am Fauzi");
 console.log("cecil");
 console.log(" Hello I am Danar");
