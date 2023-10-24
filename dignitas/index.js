139e81ee92deaece1ec7dda6f2bc70c5cf147329
Danareee <muhdankah22@gmail.com>
2023-10-24 11:17:32 +0700

    chore: change value all log

--- dignitas/index.js
+++ dignitas/index.js
@@ -1,4 +1,4 @@
-console.log('HAI');
-console.log('DIGNITASS');
-console.log('HAIII');
-console.log("HAIII AKU DANARRRR");
+console.log('HAI AKU RAYNER');
+console.log('DIGNITASS BATCH 5');
+console.log('HAIII GESSS');
+console.log("HAIII AKU CIMENKK");
