c90559139e4f03e988751aefb63be004822e3e3e
Levina Hestiya Aryani <levinahestiya@gmail.com>
2023-10-24 11:23:30 +0700

    chore: change file index dignitas

--- dignitas/index.js
+++ dignitas/index.js
@@ -2,3 +2,7 @@ console.log('HAI AKU RAYNER');
 console.log('DIGNITASS BATCH 5');
 console.log('HAIII GESSS');
 console.log("HAIII AKU CIMENKK");
+console.log('HALO');
+console.log('KALIAN');
+console.log('SEMUA');
+console.log("KAMI DIGNITAS");
